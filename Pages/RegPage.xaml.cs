9f88a3b1432df46b6ed7f218955b1b53b706fbe2
Zvonn <lvqwerte@gmail.com>
2025-10-13 22:58:09 +0300

    Добавьте файлы проекта.

---
+++ Pages/RegPage.xaml.cs
@@ -0,0 +1,173 @@
+﻿using _122_Tsygarin;
+using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace _122_Tsygarin.Pages
+{
+    public partial class RegPage : Page
+    {
+        public RegPage()
+        {
+            InitializeComponent();
+            comboBxRole.SelectedIndex = 0; // Set default role to "User"
+        }
+
+        private void txtbxLog_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            lblLogHitn.Visibility = Visibility.Visible;
+            if (txtbxLog.Text.Length > 0)
+            {
+                lblLogHitn.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void txtbxFIO_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            lblFioHitn.Visibility = Visibility.Visible;
+            if (txtbxFIO.Text.Length > 0)
+            {
+                lblFioHitn.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void passBxFrst_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            lblPassHitn.Visibility = Visibility.Visible;
+            if (passBxFrst.Password.Length > 0)
+            {
+                lblPassHitn.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void passBxScnd_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            lblPassSecHitn.Visibility = Visibility.Visible;
+            if (passBxScnd.Password.Length > 0)
+            {
+                lblPassSecHitn.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void lblLogHitn_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            txtbxLog.Focus();
+        }
+
+        private void lblPassHitn_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            passBxFrst.Focus();
+        }
+
+        private void lblPassSecHitn_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            passBxScnd.Focus();
+        }
+
+        private void lblFioHitn_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            txtbxFIO.Focus();
+        }
+
+        private void regButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Check if any field is empty
+            if (string.IsNullOrEmpty(txtbxLog.Text) ||
+                string.IsNullOrEmpty(txtbxFIO.Text) ||
+                string.IsNullOrEmpty(passBxFrst.Password) ||
+                string.IsNullOrEmpty(passBxScnd.Password))
+            {
+                MessageBox.Show("Заполните все поля!");
+                return;
+            }
+
+            // Check if login already exists in the database
+            using (Tsygarin_DB_PaymentEntities db = new Tsygarin_DB_PaymentEntities())
+            {
+                var user = db.User
+                    .AsNoTracking()
+                    .FirstOrDefault(u => u.Login == txtbxLog.Text);
+
+                if (user != null)
+                {
+                    MessageBox.Show("Пользователь с таким логином уже существует!");
+                    return;
+                }
+            }
+
+            // Password validation
+            if (passBxFrst.Password.Length < 6)
+            {
+                MessageBox.Show("Пароль слишком короткий, должно быть минимум 6 символов!");
+                return;
+            }
+
+            bool en = true;
+            bool number = false;
+
+            for (int i = 0; i < passBxFrst.Password.Length; i++)
+            {
+                if (passBxFrst.Password[i] >= '0' && passBxFrst.Password[i] <= '9')
+                    number = true;
+                else if (!((passBxFrst.Password[i] >= 'A' && passBxFrst.Password[i] <= 'Z') ||
+                           (passBxFrst.Password[i] >= 'a' && passBxFrst.Password[i] <= 'z')))
+                    en = false;
+            }
+
+            if (!en)
+            {
+                MessageBox.Show("Используйте только английскую раскладку!");
+                return;
+            }
+            else if (!number)
+            {
+                MessageBox.Show("Добавьте хотя бы одну цифру!");
+                return;
+            }
+
+            // Check if passwords match
+            if (en && number)
+            {
+                if (passBxFrst.Password != passBxScnd.Password)
+                {
+                    MessageBox.Show("Пароли не совпадают!");
+                    return;
+                }
+            }
+
+            // Save user to database
+            using (Tsygarin_DB_PaymentEntities db = new Tsygarin_DB_PaymentEntities())
+            {
+                User userObject = new User
+                {
+                    FIO = txtbxFIO.Text,
+                    Login = txtbxLog.Text,
+                    Password = GetHash(passBxFrst.Password),
+                    Role = comboBxRole.Text
+                };
+                db.User.Add(userObject);
+                db.SaveChanges();
+                MessageBox.Show("Пользователь успешно зарегистрирован!");
+                txtbxLog.Clear();
+                passBxFrst.Clear();
+                passBxScnd.Clear();
+                comboBxRole.SelectedIndex = 0;
+                txtbxFIO.Clear();
+            }
+        }
+        public static string GetHash(String password)
+        {
+            using (var hash = SHA1.Create())
+            {
+                return
+                string.Concat(hash.ComputeHash(Encoding.UTF8.GetBytes(password)).Select(x =>
+                x.ToString("X2")));
+            }
+        }
+    }
+
+}
---
+++ StartWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using _122_Tsygarin.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _122_Tsygarin
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new AuthPage());
+
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.IsEnabled = true;
+            timer.Tick += (o, t) => {
+                DateTimeNow.Text = DateTime.Now.ToString();
+            };
+            timer.Start();
+        }
+        void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (MessageBox.Show("Вы уверены, что хотите закрыть окно?", "Message",
+              MessageBoxButton.YesNo) == System.Windows.MessageBoxResult.No)
+                e.Cancel = true;
+            else
+
+                e.Cancel = false;
+        }
+        private void ThemeSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ThemeSelector.SelectedIndex == 0)
+                ChangeTheme("Dictionary.xaml");        // Светлая тема
+            else
+                ChangeTheme("DictionaryDark.xaml");    // Тёмная тема
+        }
+
+        private void ChangeTheme(string dictionaryName)
+        {
+            var uri = new Uri(dictionaryName, UriKind.Relative);
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+        }
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                MainFrame.GoBack();
+            }
+            else
+            {
+                MessageBox.Show("Нет страниц для возврата назад.");
+            }
+        }
+
+    }
+}
